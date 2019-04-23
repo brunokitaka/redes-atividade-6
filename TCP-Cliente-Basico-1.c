ef74400230fdeca1913e0685861dbd5b158079f2
brunokitaka <brunokitaka@gmail.com>
2019-04-23 18:03:16 -0300

    Add files via upload

---
+++ TCP-Cliente-Basico-1.c
@@ -0,0 +1,95 @@
+﻿#include <stdio.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netdb.h>
+#include <math.h>
+#include <stdio_ext.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <time.h>
+
+/*
+ * Cliente TCP
+ */
+int main(int argc, char *argv[]) {
+    unsigned short port;
+    FILE *file;
+    char sendbuf[150];
+    char recvbuf[1500];
+    struct hostent *hostnm;
+    struct sockaddr_in server;
+    int s, x;
+    float temp = 0.0;
+    char command[] = "cat /sys/bus/w1/devices/28-0516a46321ff/w1_slave | sed -n \'s/^.*\\(t=[^ ]*\\).*/\\1/p\' | sed \'s/t=//\' | awk \'{x=$1}END{print(x/1000)}\'";
+    
+
+    /*
+     * O primeiro argumento (argv[1]) eh o hostname do servidor.
+     * O segundo argumento (argv[2]) eh a porta do servidor.
+     */
+    if (argc != 3)
+    {
+        fprintf(stderr, "Use: %s hostname porta\n", argv[0]);
+        exit(1);
+    }
+    /*
+     * Obtendo o endereco IP do servidor
+     */
+    hostnm = gethostbyname(argv[1]);
+    if (hostnm == (struct hostent *)0)
+    {
+        fprintf(stderr, "Gethostbyname failed\n");
+        exit(2);
+    }
+    port = (unsigned short)atoi(argv[2]);
+
+    /*
+     * Define o endereco IP e a porta do servidor
+     */
+    server.sin_family = AF_INET;
+    server.sin_port = htons(port);
+    server.sin_addr.s_addr = *((unsigned long *)hostnm->h_addr);
+
+    /*
+     * Cria um socket TCP (stream)
+     */
+    if ((s = socket(PF_INET, SOCK_STREAM, IPPROTO_TCP)) < 0)
+    {
+        perror("\nERRO ao declarar o socket\n");
+        exit(3);
+    }
+
+    /* Estabelece conexao com o servidor */
+    if (connect(s, (struct sockaddr *)&server, sizeof(server)) < 0)
+    {
+        perror("\nERRO ao conectar com o servidor\n");
+        exit(4);
+    }
+
+    while (1) {
+        
+        if((file = popen(command, "r")) != NULL){
+			x = fread(sendbuf, sizeof(char), 2000, file);
+			sendbuf[x] = '\0';
+			printf("%s", sendbuf);
+		}
+        
+        if (send(s, sendbuf, strlen(sendbuf) + 1, 0) < 0) {
+            perror("\nERRO ao enviar a mensagem(1)\n");
+            exit(5);
+        }
+
+        /* Recebe a mensagem do servidor no buffer de recep��o */
+        if (recv(s, recvbuf, sizeof(recvbuf), 0) < 0) {
+            perror("\nERRO ao receber a mensagem(1)\n");
+            exit(6);
+        }
+        sleep(1);
+        printf("%s\n", recvbuf);
+        
+    }
+    close(s);
+    
+}
---
+++ TCP-Servidor-Thread.c
@@ -0,0 +1,181 @@
+/*
+	Codigo baseado em http://www.circuitbasics.com/raspberry-pi-ds18b20-temperature-sensor-tutorial/
+*/
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <string.h>
+#include <unistd.h>
+#include <pthread.h>
+
+// #include <signal.h>
+// #include <sys/ipc.h> /* for all IPC function calls */
+// #include <sys/shm.h> /* for shmget(), shmat(), shmctl() */
+
+// #define SHM_KEY 0x1434	/* SHM = Shared Memory */
+// #define BUFFER_KEY 0x1435 /* Segunda Tarefa */
+
+typedef struct
+{
+	char user[20];
+	char msg[80];
+	int flag;
+} db;
+
+db db1[10];
+
+int count;
+
+void *atendeCliente(void* param);
+
+/*
+ * Servidor TCP
+ */
+
+int main(int argc, char *argv[])
+{
+	unsigned short port;
+	struct sockaddr_in client;
+	struct sockaddr_in server;
+	int s;  /* Socket para aceitar conex�es       */
+	int ns; /* Socket conectado ao cliente        */
+	int namelen;
+	int i = 0;
+	int qtd = 0;
+	int *param;
+	pthread_t thread_id;
+
+	/*
+     * O primeiro argumento (argv[1]) eh a porta
+     * onde o servidor aguardarah por conexoes
+     */
+	if (argc != 2)
+	{
+		fprintf(stderr, "Use: %s porta\n", argv[0]);
+		exit(1);
+	}
+
+	port = (unsigned short)atoi(argv[1]);
+
+	/*
+     * Cria um socket TCP (stream) para aguardar conex�es
+     */
+	if ((s = socket(PF_INET, SOCK_STREAM, IPPROTO_TCP)) < 0)
+	{
+		perror("\nERRO ao atribuir o socket\n");
+		exit(2);
+	}
+
+	/*
+    * Define a qual endereco IP e porta o servidor estarah ligado.
+    * IP = INADDDR_ANY -> faz com que o servidor se ligue em todos
+    * os enderecos IP
+    */
+	server.sin_family = AF_INET;
+	server.sin_port = htons(port);
+	server.sin_addr.s_addr = INADDR_ANY;
+
+	/*
+     * Liga o servidor a porta definida anteriormente.
+     */
+	if (bind(s, (struct sockaddr *)&server, sizeof(server)) < 0)
+	{
+		perror("\nERRO ao dar bind na porta\n");
+		exit(3);
+	}
+
+	/*
+     * Prepara o socket para aguardar por conex�es e
+     * cria uma fila de conex�es pendentes.
+     */
+	if (listen(s, 1) != 0)
+	{
+		perror("\nERRO ao dar listen\n");
+		exit(4);
+	}
+
+	/*
+     * Aceita uma conexao e cria um novo socket atraves do qual
+     * ocorrera comunicacao com o cliente.
+     */
+	do
+	{
+		namelen = sizeof(client);
+		if ((ns = accept(s, (struct sockaddr *)&client, (unsigned int *)&namelen)) == -1)
+		{
+			perror("Accept()");
+			exit(5);
+		}
+		printf("%i\n", ns);
+		param = (int *)(long)ns;
+		/* Criar thread */
+		/* (id, null, function, params)*/
+		pthread_create(&thread_id, NULL, atendeCliente, (void *)param);
+
+		printf("Thread criada: %li\n", (long int)thread_id);
+
+		/* Fecha o socket conectado ao cliente */
+		//pthread_join(thread_id[i], NULL);
+		i++;
+		//close(ns);
+	} while (1);
+	/* Fecha o socket aguardando por conex�es */
+	close(s);
+	/* Fecha o socket conectado ao cliente */
+	close(ns);
+
+	printf("\nServidor finalizado com sucesso.\n");
+	pthread_exit(NULL);
+}
+
+void *atendeCliente(void *param)
+{
+	int z, i;
+	char recvbuf[150];
+	char sendbuf[1000];
+	char *user;
+	char *msg;
+	char usuario[100];
+	char mensagem[100];
+
+	long int tid = (long int)pthread_self();
+
+	user = NULL;
+	msg = NULL;
+
+	printf("Dentro da thread %li\n", tid);
+	printf("%li\n", (long int)param);
+	do
+	{
+		/* Recebe uma mensagem do cliente atraves do novo socket conectado */
+		z = recv((long int)param, recvbuf, sizeof(recvbuf), 0);
+		if (z != -1 && z != 0)
+		{
+				printf("Mensagem recebida do cliente: %s -- %i bytes\n", recvbuf, z);
+				strcpy(sendbuf, "");
+				
+				strcpy(sendbuf, "\nSalvo com sucesso!\n");
+				/* Envia uma mensagem ao cliente atraves do socket conectado */
+				if (send((long int)param, sendbuf, strlen(sendbuf) + 1, 0) < 0)
+				{
+					perror("Send()");
+					exit(7);
+				}
+				printf("Mensagem enviada ao cliente: %s\n", sendbuf);
+				printf("=====================================================================\n");
+			
+		}
+		else
+		{
+			perror("Recv()");
+			//exit(6);
+		}
+	} while (z != -1 && z != 0);
+
+	printf("suspender filho %li\n", tid);
+	close((long int)param);
+	pthread_exit(NULL);
+}
